1ef5b1970a068812d969f0e4917d0360aece3b26
1K2S <51541614+1Kill2Steal@users.noreply.github.com>
2024-04-16 11:40:51 +0300

    Cleaned up the code structure

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,9 @@ fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
-    let mut first_led = (0u8, pins.d10.into_output());
-    let mut second_led = (1u8, pins.d11.into_output());
-    let mut third_led = (2u8, pins.d12.into_output());
+    let mut first_led = pins.d10.into_output();
+    let mut second_led = pins.d11.into_output();
+    let mut third_led = pins.d12.into_output();
 
     first_led.1.set_low();
     second_led.1.set_low();
@@ -26,29 +26,27 @@ fn main() -> ! {
         }
 
         // Match bitwise operations on the leds using their binary index.
-        match (counter >> first_led.0) & 1 {
-            0 => first_led.1.set_low(),
-            1 => first_led.1.set_high(),
+        match counter & 1 {
+            0 => first_led.set_low(),
+            1 => first_led.set_high(),
             _ => {
                 panic!("You shouldn't have a bitwise operation with the index return something other than 0 or 1");
             }
         }
-        match (counter >> second_led.0) & 1 {
-            0 => second_led.1.set_low(),
-            1 => second_led.1.set_high(),
+        match (counter >> 1) & 1 {
+            0 => second_led.set_low(),
+            1 => second_led.set_high(),
             _ => {
                 panic!("You shouldn't have a bitwise operation with the index return something other than 0 or 1");
             }
         }
-        match (counter >> third_led.0) & 1 {
-            0 => third_led.1.set_low(),
-            1 => third_led.1.set_high(),
+        match (counter >> 2) & 1 {
+            0 => third_led.set_low(),
+            1 => third_led.set_high(),
             _ => {
                 panic!("You shouldn't have a bitwise operation with the index return something other than 0 or 1");
             }
         }
-
-        // Initialize booleans
 
         // Wait before iterating again.
         arduino_hal::delay_ms(1000);
